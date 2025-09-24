9dda237df57693a2b9a58b308d00aa183bfef5f6
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-09-23 23:04:46 -0700

    refactor: replace assistant_threads_setStatus method with assistant class set_status

--- listeners/assistant/assistant.py
+++ listeners/assistant/assistant.py
@@ -1,6 +1,7 @@
 import logging
-from typing import List, Dict
-from slack_bolt import Assistant, BoltContext, Say, SetSuggestedPrompts
+from typing import Dict, List
+
+from slack_bolt import Assistant, BoltContext, Say, SetStatus, SetSuggestedPrompts
 from slack_bolt.context.get_thread_context import GetThreadContext
 from slack_sdk import WebClient
 
@@ -53,11 +54,12 @@ def start_assistant_thread(
 # This listener is invoked when the human user sends a reply in the assistant thread
 @assistant.user_message
 def respond_in_assistant_thread(
-    payload: dict,
-    logger: logging.Logger,
-    context: BoltContext,
     client: WebClient,
+    context: BoltContext,
+    logger: logging.Logger,
+    payload: dict,
     say: Say,
+    set_status: SetStatus,
 ):
     try:
         channel_id = payload["channel"]
@@ -83,8 +85,9 @@ def respond_in_assistant_thread(
             messages_in_thread.append({"role": role, "content": message["text"]})
 
         returned_message = call_llm(messages_in_thread)
-        client.assistant_threads_setStatus(
-            channel_id=channel_id, thread_ts=thread_ts, status="Bolt is typing", loading_messages=loading_messages
+        set_status(
+            status="Bolt is typing",
+            loading_messages=loading_messages,
         )
         stream_response = client.chat_startStream(
             channel=channel_id,
