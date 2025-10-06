68b56c28f3362252d5d7d58729ba5e2d19939bdf
Michael Brooks <michael@michaelbrooks.ca>
2025-10-06 13:12:43 -0700

    fix: add support for streaming messages in enterprise orgs (#20)

--- listeners/assistant/assistant.py
+++ listeners/assistant/assistant.py
@@ -67,9 +67,9 @@ def respond_in_assistant_thread(
 ):
     try:
         channel_id = payload["channel"]
-        team_id = payload["team"]
+        team_id = context.team_id
         thread_ts = payload["thread_ts"]
-        user_id = payload["user"]
+        user_id = context.user_id
         user_message = payload["text"]
 
         set_status(
