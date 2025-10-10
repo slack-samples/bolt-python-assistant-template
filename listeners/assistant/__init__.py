922108054e13f6b5f8ad11738a9c2e4bcd58cab8
Ale Mercado <maria.mercado@slack-corp.com>
2025-10-10 11:10:35 -0400

    ref: breakup assistant.py into separate files

--- listeners/assistant/__init__.py
+++ listeners/assistant/__init__.py
@@ -1,7 +1,14 @@
-from slack_bolt import App
+from slack_bolt import App, Assistant
 
-from .assistant import assistant
+from .assistant_thread_started import start_assistant_thread
+from .message import respond_in_assistant_thread
 
 
+# Refer to https://tools.slack.dev/bolt-python/concepts/assistant/ for more details on the Assistant class
 def register(app: App):
+    assistant = Assistant()
+
+    assistant.thread_started(start_assistant_thread)
+    assistant.user_message(respond_in_assistant_thread)
+
     app.assistant(assistant)
